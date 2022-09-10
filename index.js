25026fbaf2caf848a7dc45b16526b7f42be6bbe5
GasFeDev <gasfedev@gmail.com>
2022-09-10 10:43:58 -0300

    deploy 2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
-/* app.use("/images", express.static(path.join(__dirname, "public/images"))); */
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 /* Cuando usamos app.use(), le estamos diciendo a Express que use un Middleware, el Middleware básicamente es un bloque de código o función que se ejecuta entre la petición que hace el usuario hasta que la petición llega al servidor, funciona como una especie de proxy.
 express.static() busca y devuelve los archivos estáticos solicitados por el usuario y recibe como parámetro la ruta del folder donde están esos archivos.
 Utilizamos el modulo path para proporcionar una ruta absoluta de la carpeta, ya que se colocas la ruta directamente estarías proporcionando una ruta relativa y si ejecutas la aplicación desde otro directorio o similar puedes tener problemas, por ello se recomienda utilizar la ruta absoluta y módulo path nos ayuda con eso. */
@@ -37,23 +37,23 @@ Morgan es un Middleware de nivel de solicitud HTTP. Es una gran herramienta que 
 Explicación: Básicamente, configuramos Morgan, y dado que es un middleware, usamos el método .use() para decirle a express que lo use como middleware en nuestra aplicación. */
 
 /* Multer es un "middleware" de node.js para el manejo de multipart/form-data, el cuál es usado sobre todo para la subida de archivos.  */
-/* const storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/images");
   },
   filename: (req, file, cb) => {
     cb(null, req.body.name);
   },
-}); */
+});
 
-/* const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
     return res.status(200).json("File uploded successfully");
   } catch (error) {
     console.error(error);
   }
-}); */
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
